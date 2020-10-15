32c23b8d0621896848038387beea225662c86498
alkotras42 <kirill.stogniy@mail.ru>
2020-10-15 21:52:15 +0300

    add auth with validation

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,11 @@
+const postsResolvers = require('./post');
+const userResolvers = require('./users')
+
+module.exports = {
+    Query: {
+        ...postsResolvers.Query
+    },
+    Mutation: {
+        ...userResolvers.Mutation
+    }
+};
--- index.js
+++ index.js
@@ -1,35 +1,10 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
+
 const { MONGODB } = require('./config.js');
-
-const Post = require('./models/Post');
-
-const typeDefs = gql`
-    type Post{
-        id: ID!
-        body: String!
-        username: String!
-        createdAt: String!
-    }
-    type Query{
-        getPosts: [Post]
-    }
-`
-
-const resolvers = {
-    Query: {
-        async getPosts(){
-            try{
-                const posts = await Post.find();
-                return posts;
-            }
-            catch (error){
-                throw new Error(error);
-            }
-        }
-    }
-};
+const resolvers = require('./graphql/resolvers')
+const typeDefs = require('./graphql/typeDefs')
 
 const server = new ApolloServer({
     typeDefs,
