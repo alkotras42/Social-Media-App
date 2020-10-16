5861fc886ccd3c7dc0d949600be00040b5153de5
alkotras42 <kirill.stogniy@mail.ru>
2020-10-17 00:05:36 +0300

    add create comment resolver

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,33 @@
+const Post = require("../../models/Post");
+const { UserInputError } = require("apollo-server");
+
+const checkAuth = require("../../util/check-auth");
+
+module.exports = {
+  Mutation: {
+    createComment: async (_, { postId, body }, context) => {
+      const { username } = checkAuth(context);
+      if (body.trim() === "") {
+        throw new UserInputError("Message is empty", {
+          error: {
+            body: "Comment body must not be empy",
+          },
+        });
+      }
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        post.comments.unshift({
+          body,
+          username,
+          createdAt: new Date().toISOString(),
+        });
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+  },
+};
