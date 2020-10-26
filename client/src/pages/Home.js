bd420d4e96bbaa3bd5c78ac335ab25889dd015d4
alkotras42 <kirill.stogniy@mail.ru>
2020-10-26 17:35:43 +0300

    add like button functional

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,18 @@
-import React, {useContext} from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { Grid } from 'semantic-ui-react';
+import React, { useContext } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { Grid, Transition } from 'semantic-ui-react'
 
-import {AuthContext} from '../context/auth'
-import PostCard from '../components/PostCard';
+import { AuthContext } from '../context/auth'
+import PostCard from '../components/PostCard'
 import PostForm from '../components/PostForm'
-import {FETCH_POSTS_QUERY} from '../util/graphql'
+import { FETCH_POSTS_QUERY } from '../util/graphql'
 
 function Home() {
-  const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext)
   const {
     loading,
-    data: { getPosts: posts }
-  } = useQuery(FETCH_POSTS_QUERY);
+    data: { getPosts: posts },
+  } = useQuery(FETCH_POSTS_QUERY)
 
   return (
     <Grid columns={3}>
@@ -22,24 +22,24 @@ function Home() {
       <Grid.Row>
         {user && (
           <Grid.Column>
-            <PostForm>
-
-            </PostForm>
+            <PostForm></PostForm>
           </Grid.Column>
         )}
         {loading ? (
           <h1>Loading posts..</h1>
         ) : (
-          posts &&
-          posts.map((post) => (
-            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-              <PostCard post={post} />
-            </Grid.Column>
-          ))
+          <Transition.Group>
+            {posts &&
+              posts.map((post) => (
+                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                  <PostCard post={post} />
+                </Grid.Column>
+              ))}
+          </Transition.Group>
         )}
       </Grid.Row>
     </Grid>
-  );
+  )
 }
 
-export default Home;
+export default Home
