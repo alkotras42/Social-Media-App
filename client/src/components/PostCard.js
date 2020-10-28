bbec347945ffb91292f8332a2daf5693e8f034f8
alkotras42 <kirill.stogniy@mail.ru>
2020-10-29 00:30:21 +0300

    add delete button functionality and single post pages

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -5,12 +5,12 @@ import moment from 'moment'
 
 import { AuthContext } from '../context/auth'
 import LikeButton from './LikeButton'
+import DeleteButton from './DeleteButton'
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) {
   const { user } = useContext(AuthContext)
-
 
   return (
     <Card fluid>
@@ -36,17 +36,7 @@ function PostCard({
             {commentCount}
           </Label>
         </Button>
-        {user && user.username === username && (
-          <Button
-            as="div"
-            color="red"
-            floated="right"
-            basic
-            onClick={() => console.log('Delete post')}
-          >
-            <Icon name="trash" style={{ margin: 0 }}></Icon>
-          </Button>
-        )}
+        {user && user.username === username && <DeleteButton postId={id}/>}
       </Card.Content>
     </Card>
   )
